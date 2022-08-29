73cf5aac61cd93e7a198d5af43d760cfaf1fcbfe
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-29 09:50:16 +0200

    Implementing crypto display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import CryptoContainer from './components/CryptoContainer';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<CryptoContainer />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
